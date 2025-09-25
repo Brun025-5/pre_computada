9a3fffe7d879f5feeeddfbd348bc12107f8b37db
Brun025 <brurrome@espol.edu.ec>
2025-09-24 20:28:20 -0500

    chore: 3 of 4 tables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,190 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="bg-slate-50 min-h-screen p-4 sm:p-6 lg:p-8">
+      <div className="max-w-7xl mx-auto">
+        <header className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-800">Pre Computada</h1>
+          <p className="text-slate-500">Introduce los datos para calcular.</p>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+        {/* Área Principal Derecha: La Tabla */}
+        <div className="flex flex-col gap-5">
+          <Card className="p-0">
+            <CardContent className="px-0">
+              <Table>
+                <TableHeader>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" rowSpan={2} colSpan={2}>Check Points</TableHead>
+                    <TableHead className="text-center" colSpan={2}>VOR</TableHead>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r">Frecuencia</TableHead>
+                    <TableHead className="text-center">Identificación</TableHead>
+                  </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                  <TableRow className="hover:bg-default">
+                    <TableCell>
+                      <Input id="checkpoint1" type="text" placeholder="CP1" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="checkpoint2" type="text" placeholder="CP2" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="frequency" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="identification" type="number" placeholder="42.0" />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+
+          <Card className="p-0">
+            <CardContent className="px-0">
+              <Table>
+                <TableHeader>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" rowSpan={3}>Course</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={3}>Altitud</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={1} colSpan={2}>Wind</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={3}>CAS</TableHead>
+                    <TableHead className="text-center" rowSpan={3}>TAS</TableHead>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" rowSpan={1}>Dirección</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={1}>Velocidad</TableHead>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" colSpan={2}>Temperatura</TableHead>
+                  </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                  <TableRow className="hover:bg-default">
+                    <TableCell rowSpan={2}>
+                      <Input id="course" type="number" placeholder="num" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Input id="altitude" type="number" placeholder="" />
+                    </TableCell>
+                    <TableCell rowSpan={1}>
+                      <Input id="direction" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={1}>
+                      <Input id="speed" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Input id="CAS" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Label id="TAS">TAS</Label>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableCell rowSpan={1} colSpan={2}>
+                      <Input id="temperature" type="number" placeholder="[°]" />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+
+          <Card className="p-0">
+            <CardContent className="px-0">
+              <Table>
+                <TableHeader>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r">TC</TableHead>
+                    <TableHead className="text-center border-r">TH</TableHead>                      
+                    <TableHead className="text-center border-r">MH</TableHead>
+                    <TableHead className="text-center">CM</TableHead>
+                  </TableRow>
+                </TableHeader>
+
+                <TableBody>  
+                  <TableRow className="hover:bg-default">
+                    <TableCell>
+                      <Input id="TC" type="text" placeholder="x" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="TH" type="text" placeholder="x" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="MH" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell>
+                      <Input id="CM" type="number" placeholder="42.0" />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+
+          <Card className="p-0">
+            <CardContent className="px-0">
+              <Table>
+                <TableHeader>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" rowSpan={3}>Distancia</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={3}>Altitud</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={1} colSpan={2}>Wind</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={3}>CAS</TableHead>
+                    <TableHead className="text-center" rowSpan={3}>TAS</TableHead>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" rowSpan={1}>Dirección</TableHead>
+                    <TableHead className="text-center border-r" rowSpan={1}>Velocidad</TableHead>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableHead className="text-center border-r" colSpan={2}>Temperatura</TableHead>
+                  </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                  <TableRow className="hover:bg-default">
+                    <TableCell rowSpan={2}>
+                      <Input id="course" type="number" placeholder="num" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Input id="altitude" type="number" placeholder="" />
+                    </TableCell>
+                    <TableCell rowSpan={1}>
+                      <Input id="direction" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={1}>
+                      <Input id="speed" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Input id="CAS" type="number" placeholder="42.0" />
+                    </TableCell>
+                    <TableCell rowSpan={2}>
+                      <Label id="TAS">TAS</Label>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow className="hover:bg-default">
+                    <TableCell rowSpan={1} colSpan={2}>
+                      <Input id="temperature" type="number" placeholder="[°]" />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
